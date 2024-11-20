4d20766a34ec1541039edbe5b1d677dbba0fe530
Pablo Angusto <pabloangusto@gmail.com>
2024-11-20 17:16:59 +0000

    Refactor server request handling to support multipart messages; add configuration for seed nodes; improve request forwarding logic

--- server/server.py
+++ server/server.py
@@ -12,6 +12,9 @@ import pdb
 
 id = int(sys.argv[1])
 port = 5500 + id
+W = 2
+R = 2
+
 
 active_lists = {}
 active_nodes = {}
@@ -21,19 +24,21 @@ def hash_list_id(list_id):
     print("Hashing list id", hashlib.md5(list_id.encode()))
     return int(hashlib.md5(list_id.encode()).hexdigest(), 16)
 
-def forward_request(id_node, client_shopping_list):
+def forward_request(id_node, client_shopping_list, message_multipart):
     print("Forwarding request")
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
     socket.connect(f"tcp://localhost:{5500+id_node}")
     message = client_shopping_list.encode()
     print("Sending message to server: " + str(id_node) + message )
-    socket.send_string(message)
-    message = socket.recv_string()
-    print(f"Received response: {message}")
+    socket.send_multipart([message_multipart[0],b'', message.encode()])
+    response = socket.recv_string()
+    print(f"Received response: {response}")
 
-def request_received(socket, message):
-    print("Received request")
+def request_received(socket, message_multipart):
+
+    message = message_multipart[2]
+    print("Received request", message)
 
     client_shopping_list = ShoppingList()
     client_shopping_list.decode(message.decode())
@@ -42,26 +47,26 @@ def request_received(socket, message):
     id_node = hash_list_id(client_shopping_list.list) % len(active_nodes)
     # pdb.set_trace()
 
-    if id == id_node:
+    if id >= id_node and id < id_node + W:
 
         if client_shopping_list.list not in active_lists:
             active_lists[client_shopping_list.list] = client_shopping_list
             print("Sending not changed message to server")
-            socket.send(b"Your list haven't changed.\n")
+            response = "Your list haven't changed.\n"
+            socket.send_multipart([message_multipart[0],b'', response.encode()])
 
         else:
 
             active_lists[client_shopping_list.list].merge(client_shopping_list)
-            message = active_lists[client_shopping_list.list].encode()
+            response = active_lists[client_shopping_list.list].encode()
             print("Sending message to server: " + message)
-            socket.send_string(message)
+            socket.send_multipart([message_multipart[0],b'', response.encode()])
 
 
         print(active_lists[client_shopping_list.list])
     else:
         print("Forwarding request")
-        forward_request(id_node, client_shopping_list)
-        socket.send(b"wait")
+        forward_request(id_node, client_shopping_list, message_multipart)
 
 
 
@@ -115,13 +120,15 @@ def node_request():
     socket3.bind("tcp://localhost:"+str(port))
     while True:
         print("Server node listening: " + str(port))
-        message = socket3.recv()
+        message = socket3.recv_multipart()
         print(f"Received request: {message}")
         socket3.send(b"ok")
+        update_thread = threading.Thread(target=request_received, args=(socket, message))
+        update_thread.start()
         # request_received(socket, message)
 
 context = zmq.Context()
-socket = context.socket(zmq.REP)
+socket = context.socket(zmq.DEALER)
 socket.connect("tcp://localhost:5560")
 print("Server started")
 # update_thread = threading.Thread(target=send_updates)
@@ -134,7 +141,8 @@ update_thread = threading.Thread(target=node_request)
 update_thread.start()
 hash_list_id("test")
 while True:
-    message = socket.recv()
+    message = socket.recv_multipart()
     print(f"Received request: {message}")
     request_received(socket, message)
+
 
